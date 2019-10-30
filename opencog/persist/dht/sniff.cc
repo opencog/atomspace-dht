13ee0dc96821a3ad62241b3a01e970b397d83208
Linas Vepstas <linasvepstas@gmail.com>
2019-10-30 15:15:53 -0500

    Misc hacking

--- opencog/persist/dht/sniff.cc
+++ opencog/persist/dht/sniff.cc
@@ -2,6 +2,7 @@
  See if anything works.
  */
 
+#include <unistd.h>
 #include <iostream>
 #include <opendht.h>
 
@@ -11,17 +12,48 @@ int main (int, const char **)
 
 	// Launch a dht node on a new thread, using a
 	// generated RSA key pair, and listen on port 4222.
-	node.run(4222, dht::crypto::generateIdentity(), true);
+	node.run(4223, dht::crypto::generateIdentity(), true);
 
 	// Join the network through any running node,
 	// here using a known bootstrap node.
-	node.bootstrap("bootstrap.ring.cx", "4222");
+	// node.bootstrap("bootstrap.ring.cx", "4222");
+	node.bootstrap("localhost", "4222");
 
+	std::cout << "My node id: " << node.getId() << std::endl;
+	std::cout << "My DHT node id: " << node.getNodeId() << std::endl;
+	// std::cout << "Node info: " << node.getNodeInfo() << std::endl;
+
+	dht::InfoHash akey = dht::InfoHash::get("atom_key");
+	std::cout << "Key's this: " << akey << std::endl;
+#if 0
 	// put some data on the dht
-	node.put("atom_key", "(some test stuff)");
+	node.put(akey, "(some test stuff)");
+printf("duude put stuff\n");
+#endif
 
 	// put some data on the dht, signed with our generated private key
 	// node.putSigned("unique_key_42", some_data);
+
+	// get data from the dht
+	dht::GetCallback mycb =
+		[](const std::vector<std::shared_ptr<dht::Value>>& values)
+		{
+			std::cout << "Num values found= " << values.size() << std::endl;
+			// Callback called when values are found
+			for (const auto& value : values)
+				std::cout << "Found value: " << *value << std::endl;
+			return true; // return false to stop the search
+		};
+
+	printf("sleep\n");
+	sleep(10);
+	printf("wake\n");
+	node.get(akey, mycb);
+	printf("and again\n");
+	node.get("atom_key", mycb);
+printf("duude got stuff\n");
+
+	std::cout << "Searches: " << node.getSearchesLog() << std::endl;
 
 #if 0
 	// get data from the dht
